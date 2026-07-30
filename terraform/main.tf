7510ccddca60975e087ab26700205bc3935dd727
Kohei Watanabe <sek.ohei.w0822@icloud.com>
2026-07-31 02:57:33 +0900

    add: collaboratorの招待

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,8 @@ resource "github_repository" "nuage_workspace" {
   name        = "nuage-workspace"
   description = ""
 }
+
+resource "github_repository_collaborator" "nuage_workspace_collaborator" {
+  repository = github_repository.nuage_workspace.name
+  username   = "bot-wa-wa"
+}
