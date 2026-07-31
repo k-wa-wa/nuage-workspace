18fe0924a98ad613d9f4ec7f9fdb47819085726a
Kohei Watanabe <sek.ohei.w0822@icloud.com>
2026-07-31 22:53:48 +0900

    add: 全リポジトリを対象とする

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,73 @@ module "nuage_workspace" {
     description = ""
   }
 }
+
+#####
+
+import {
+  id = "nuage-cluster"
+  to = module.nuage_cluster.github_repository.repository
+}
+module "nuage_cluster" {
+  source = "./modules/repo"
+  repository_config = {
+    name        = "nuage-cluster"
+    description = "おうちクラスターのセットアップリポジトリ"
+  }
+}
+
+#####
+
+import {
+  id = "nuage-monitoring-stack"
+  to = module.nuage_monitoring_stack.github_repository.repository
+}
+module "nuage_monitoring_stack" {
+  source = "./modules/repo"
+  repository_config = {
+    name        = "nuage-monitoring-stack"
+    description = "おうちクラスターのモニタリング環境"
+  }
+}
+
+#####
+
+import {
+  id = "pechka"
+  to = module.pechka.github_repository.repository
+}
+module "pechka" {
+  source = "./modules/repo"
+  repository_config = {
+    name        = "pechka"
+    description = ""
+  }
+}
+
+#####
+
+import {
+  id = "bare-web-proxy"
+  to = module.bare_web_proxy.github_repository.repository
+}
+module "bare_web_proxy" {
+  source = "./modules/repo"
+  repository_config = {
+    name        = "bare-web-proxy"
+    description = "通信制限を救う超軽量なプロキシ"
+  }
+}
+
+#####
+
+import {
+  id = "nuage-autopilot"
+  to = module.nuage_autopilot.github_repository.repository
+}
+module "nuage_autopilot" {
+  source = "./modules/repo"
+  repository_config = {
+    name        = "nuage-autopilot"
+    description = "Issue駆動の完全自律開発エージェント"
+  }
+}
