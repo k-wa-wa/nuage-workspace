90a5ef2ec3a903984b4557f692e641fbab44b570
Kohei Watanabe <sek.ohei.w0822@icloud.com>
2026-07-31 21:54:13 +0900

    update: デフォルトブランチの保護

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,40 @@ resource "github_repository" "nuage_workspace" {
   has_wiki     = true
 }
 
-resource "github_repository_collaborator" "nuage_workspace_collaborator" {
+resource "github_repository_collaborators" "nuage_workspace_collaborators" {
   repository = github_repository.nuage_workspace.name
-  username   = "bot-wa-wa"
+  user {
+    username = "bot-wa-wa"
+  }
+}
+
+resource "github_repository_ruleset" "nuage_workspace_ruleset" {
+  name        = "protect-default-branch"
+  repository  = github_repository.nuage_workspace.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    actor_type  = "RepositoryRole"
+    bypass_mode = "always"
+    actor_id    = 5
+  }
+
+  rules {
+    creation = true
+    update   = true
+    deletion = true
+
+    pull_request {
+    }
+
+    non_fast_forward = true
+  }
 }
