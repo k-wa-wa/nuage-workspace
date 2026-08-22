9086c0a620f9a3e33ee8402ce0e66d91dd1aedaa
Kohei Watanabe <sek.ohei.w0822@icloud.com>
2026-08-23 01:07:24 +0900

    delete: 不要リソース削除

--- claude-ollama.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ANTHROPIC_AUTH_TOKEN=ollama \
-ANTHROPIC_API_KEY="" \
-ANTHROPIC_BASE_URL=http://192.168.5.222:11434 \
-~/.local/bin/claude --model qwen3.5:9b --dangerously-skip-permissions
